a1935e0cecf4947572ec64dca97aa94aabea8f22
siinyann <sinyanchong4@gmail.com>
2023-06-15 14:24:48 +0800

    Added Starter Codes

---
+++ routes/main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	const title = 'Video Jotter';
+	// renders views/index.handlebars, passing title as an object
+	res.render('index', { title: title })
+});
+
+router.get('/about', (req, res) => {
+	const author = 'Your Name';
+	res.render('about', { author})
+});
+
+module.exports = router;
