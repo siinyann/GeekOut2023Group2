8ca0a0caff7bd3de6422fdd5ae9d905b4d7363b7
siinyann <sinyanchong4@gmail.com>
2023-06-15 15:09:40 +0800

    Add user route codes and flash message codes

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const flashMessage = require('../helpers/messenger');
 
 router.get('/', (req, res) => {
 	const title = 'Video Jotter';
@@ -10,6 +11,23 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
 	const author = 'Your Name';
 	res.render('about', { author})
+});
+
+router.post('/flash', (req, res) => {
+	const message = 'This is an important message';
+	const error = 'This is an error message';
+	const error2 = 'This is the second error message';
+
+	// req.flash('message', message);
+	// req.flash('error', error);
+	// req.flash('error', error2);
+
+	flashMessage(res, 'success', message);
+	flashMessage(res, 'info', message);
+	flashMessage(res, 'error', error);
+	flashMessage(res, 'error', error2, 'fas fa-sign-in-alt', true);
+
+	res.redirect('/about');
 });
 
 module.exports = router;
